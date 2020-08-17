2b64672cbd9ae496d6cb3162a41ca2d9c2a7cbc6
MelaniRichi <melani_hr@hotmail.com>
2020-08-17 15:00:39 -0300

    excercise one

---
+++ src/ar/com/ada/online/second/repetitioncycles/ExcersiceOneA.java
@@ -0,0 +1,31 @@
+package ar.com.ada.online.second.repetitioncycles;
+
+import java.util.Scanner;
+
+public class ExcersiceOneA {
+
+
+    public static void main(String[] args) {
+
+        //se declaran las variables
+        int sum = 0;
+        int[] arraynumbers = new int[10];
+
+        // se habilita el uso del teclado
+        Scanner keyboard = new Scanner(System.in);
+
+        //se piden los datos
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("ingrse el valor para la posicion del %d en el arreglo: ", i);
+            arraynumbers[i] = keyboard.nextInt();
+
+        }
+        for (int i = 0; i < arraynumbers.length; i++){
+            sum += arraynumbers[i];
+        }
+
+        //Se muestra el resultado
+        System.out.println("El resultado de la suma es de: " + sum);
+
+    }
+}
